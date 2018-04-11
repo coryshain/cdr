b6db742c78acfd7e9302241b4fc3bda4c94e89b0
Cory Shain <cory.shain@gmail.com>
2018-04-10 22:13:57 -0400

    added help.py

---
+++ dtsr/bin/help.py
@@ -0,0 +1,15 @@
+import os
+
+bin_dir = os.path.dirname(os.path.realpath(__file__))
+scripts = [x[:-3] for x in os.listdir(bin_dir) if x.endswith('py') and not (x.endswith('__init__.py') or x.endswith('help.py'))]
+
+
+for s in scripts:
+    print('='*50)
+    print('PROGRAM NAME: %s' %s)
+    print('')
+    print('To run, use command:')
+    print('python -m dtsr.bin.%s [args]' %s)
+    print('')
+    os.system('python -m dtsr.bin.%s -h' %s)
+    print('')
