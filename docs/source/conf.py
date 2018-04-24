74c9c99b54866fc9af9c5482103bdaaa4311abfe
Cory Shain <cory.shain@gmail.com>
2018-04-24 14:46:33 -0400

    tweaks to docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -25,7 +25,7 @@ class Mock(MagicMock):
     def __getattr__(cls, name):
         return MagicMock()
 
-MOCK_MODULES = ['tensorflow', 'edward']
+MOCK_MODULES = ['tensorflow', 'edward', 'edward.models']
 sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
 sys.path.insert(0, os.path.abspath('../../'))
