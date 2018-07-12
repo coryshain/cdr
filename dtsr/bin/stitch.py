8342cc6e6e53df1ae971fb07baa6dff515113a2a
coryshain <cory.shain@gmail.com>
2018-07-11 23:43:30 -0400

    Added stitch.py, a utility for stitching plots together for easy comparision

---
+++ dtsr/bin/stitch.py
@@ -0,0 +1,42 @@
+from PIL import Image, ImageDraw, ImageFont, ImageOps
+import argparse
+from dtsr.config import Config
+
+def stitch(dir_paths, filename, output_path):
+     imgs = []
+     for dir_path in dir_paths:
+         im = Image.open(dir_path + '/' + filename)
+         if im.mode == 'RGBA':
+             im = im.convert('RGB')
+         im = ImageOps.expand(im, 300, fill='white')
+         x = im.size[0]
+         pt = int(x / 50)
+         font = ImageFont.truetype("arial.ttf", pt)
+         draw = ImageDraw.Draw(im)
+         draw.text((pt,pt), dir_path.split('/')[-1], fill=(0,0,0,0), font=font)
+         imgs.append(im)
+     imgs[0].save(output_path, 'PDF', resolution=100, save_all=True, append_images=imgs[1:])
+
+
+if __name__ == '__main__':
+    argparser = argparse.ArgumentParser('''
+            Stitches plots from DTSR models into a single PDF, making it easy to page through estimated IRF.
+        ''')
+    argparser.add_argument('config_path', help='Path to configuration (*.ini) file')
+    argparser.add_argument('-m', '--models', nargs='*', default=[], help='Path to configuration (*.ini) file')
+    argparser.add_argument('-i', '--irftype', type=str, default='atomic_scaled', help='Type of plot to paste. Choose from "atomic_scaled", "atomic_unscaled", "composite_scaled", and "composite_unscaled".')
+    args, unknown = argparser.parse_known_args()
+
+    assert args.irftype in ["atomic_scaled", "atomic_unscaled", "composite_scaled", "composite_unscaled"], 'Unrecognized argument "%s" to -i/--irftype' %args.irftype
+
+    p = Config(args.config_path)
+    if len(args.models) > 0:
+        models = args.models
+    else:
+        models = p.model_list[:]
+
+    paths = []
+    for m in models:
+        paths.append(p.outdir + '/' + m)
+
+    stitch(paths, 'irf_' + args.irftype + '.png', p.outdir + '/irf_' + args.irftype + '.pdf')
