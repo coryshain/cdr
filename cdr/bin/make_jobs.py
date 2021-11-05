a94f394f6f6ceff6903fd110cd52c104f401968d
coryshain <cory.shain@gmail.com>
2021-11-05 10:40:31 -0400

    Tweak to make_jobs

--- cdr/bin/make_jobs.py
+++ cdr/bin/make_jobs.py
@@ -37,7 +37,7 @@ if __name__ == '__main__':
     use_gpu = args.use_gpu
     memory = args.memory
     slurm_partition = args.slurm_partition
-    cli_args = args.cli_args
+    cli_args = args.cli_args.replace('\\', '') # Delete escape characters
     outdir = args.outdir
 
     if not os.path.exists(outdir):
