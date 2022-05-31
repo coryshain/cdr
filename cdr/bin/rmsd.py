922332face47bf081a9f744b7c732ded9d4a215b
coryshain <cory.shain@gmail.com>
2022-05-31 12:51:50 -0400

    Updates to rmsd

--- cdr/bin/rmsd.py
+++ cdr/bin/rmsd.py
@@ -13,8 +13,11 @@ if __name__ == '__main__':
     ''')
     argparser.add_argument('paths', nargs='+', help='Path(s) to config file(s) defining experiments')
     argparser.add_argument('-m', '--models', nargs='*', default = [], help='Model names for which to compute RMSD. Regex permitted. If unspecified, uses all CDR models.')
+    argparser.add_argument('-n', '--n_samples', default=None, help='Number of samples to draw. If unspecified, use MLE/MAP.')
     argparser.add_argument('--cpu_only', action='store_true', help='Use CPU implementation even if GPU is available.')
     args = argparser.parse_args()
+
+    n_samples = args.n_samples
 
     for path in args.paths:
         p = Config(path)
@@ -43,8 +46,9 @@ if __name__ == '__main__':
 
             stderr('Computing RMSD...\n')
 
-            rmsd_mean, rmsd_lowe, rmsd_upper, rmsd_samples = cdr_model.irf_rmsd(
+            rmsd_mean, rmsd_lower, rmsd_upper, rmsd_samples = cdr_model.irf_rmsd(
                 gold_irf_lambda,
+                n_samples=n_samples
             )
 
             summary = '=' * 50 + '\n'
@@ -54,7 +58,10 @@ if __name__ == '__main__':
             summary += '  ' + formula + '\n\n'
             summary += 'Path to synth model:\n'
             summary += '  ' + synth_path + '\n\n'
-            summary += 'RMSD from gold: %s\n\n' % rmsd_mean[cdr_model.response_names[0]]['mean']
+            summary += 'RMSD from gold: %s\n'
+            summary += '  Mean:  %s' % rmsd_mean[cdr_model.response_names[0]]['mean']
+            summary += '  2.5%%:  %s' % rmsd_lower[cdr_model.response_names[0]]['mean']
+            summary += '  97.5%%: %s' % rmsd_upper[cdr_model.response_names[0]]['mean']
             summary += '=' * 50 + '\n'
 
             out_name = 'synth_rmsd'
