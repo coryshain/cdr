89a1ab2a91e7cb145ff67c29a2c088c66c9bc32a
Cory Shain <cory.shain@gmail.com>
2022-10-16 19:06:51 -0400

    Patch 0.8.3

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import find_packages
 setup(
     name='cdrnn',
     python_requires='>=3',
-    version='0.8.2',
+    version='0.8.3',
     description='A toolkit for continuous-time deconvolutional regression (CDR)',
     author='Cory Shain',
     author_email='cory.shain@gmail.com',
